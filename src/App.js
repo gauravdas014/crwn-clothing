18ea9037bfd9c852774cdc96358424428f81c8ba
Gaurav Das <gauravdas014@gmail.com>
2021-01-24 15:01:24 +0530

    scss setup and initial homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
