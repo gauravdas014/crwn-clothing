ee97a5eaa3489ffb334dd3c37735747d68d436db
Gaurav Das <gauravdas014@gmail.com>
2021-03-13 23:53:48 +0530

    add input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/Homepage/homepage.component';
 import ShopPage from './pages/Shop/shop.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 import Header from "./components/header/header.component"
 
 const HatsPage = () => (
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
