2ce005170f1ed828bd6c1f49f947ba07d2f2f17b
Gaurav Das <gauravdas014@gmail.com>
2021-03-18 00:04:38 +0530

    feat: sign in with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,10 +17,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error)
+    }
+
   };
 
   handleChange = event => {
@@ -42,7 +50,7 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             value={this.state.email}
             label='email'
-            // required
+          // required
           />
           <FormInput
             name='password'
@@ -50,7 +58,7 @@ class SignIn extends React.Component {
             value={this.state.password}
             handleChange={this.handleChange}
             label='password'
-            // required
+          // required
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
