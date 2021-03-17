1d3843b0b7e3321a197afc04cdd4a5ea0f6d560f
Gaurav Das <gauravdas014@gmail.com>
2021-03-17 12:53:52 +0530

    feat: add firebase config of sign up

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
+import {connect} from "react-redux"
+
 import {auth} from "../../firebase/firebase.utils"
 
 import { ReactComponent as Logo } from "../../assets/crown.svg"
@@ -27,4 +29,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
   measurementId: 'G-J6V8R32L5S',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
