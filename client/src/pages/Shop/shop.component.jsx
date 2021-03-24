62800b23010b8ffffab099ef6f9dda87cfb33ddc
Gaurav Das <gauravdas014@gmail.com>
2021-03-25 04:12:57 +0530

    chore: move shop data to reducer

--- client/src/pages/Shop/shop.component.jsx
+++ client/src/pages/Shop/shop.component.jsx
@@ -1,29 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from "./shop.data"
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../Collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-        this.state  = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const {collections} = this.state;
-       return (
-           <div className="shop-page">
-               {
-                   collections.map(({id, ...otherCollectionProps}) => (
-                       <CollectionPreview key={id} {...otherCollectionProps}/>
-                   ))
-               }
-           </div>
-       )
-    }
-}
-
-export default ShopPage
+export default ShopPage;
