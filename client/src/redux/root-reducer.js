7b293cc82f447a1a6e24e6efd8492957248eb8ce
Gaurav Das <gauravdas014@gmail.com>
2021-03-24 15:02:24 +0530

    feat: add redux persist for session storage

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducers';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whilelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
