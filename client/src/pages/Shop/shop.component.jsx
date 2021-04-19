baf71e955710411a95b89cd3d71db7357d6c611f
Gaurav Das <gauravdas014@gmail.com>
2021-04-19 13:24:47 +0530

    feat: add observer pattern to promise

--- client/src/pages/Shop/shop.component.jsx
+++ client/src/pages/Shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../Collection/collection.component';
 
-import {firestore, convertCollectionsSnapshotToMap} from "../../firebase/firebase.utils"
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils"
 import { updateCollections } from '../../redux/shop/shop.actions';
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component"
@@ -22,21 +22,27 @@ class ShopPage extends React.Component {
 
   unsubscribeFromSnapshot = null;
 
-  componentDidMount(){
+  componentDidMount() {
 
-    const { updateCollections} = this.props; 
+    const { updateCollections } = this.props;
 
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async snapshot => {
+
+    // use fetch to retrive data from firestore
+    // fetch("https://firestore.googleapis.com/v1/projects/crwn-clothing-7e14a/databases/(default)/documents/collections")
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections))
+
+    collectionRef.get().then((snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
-      this.setState({loading: false})
-    })
+      this.setState({ loading: false })
+    }));
   }
 
-  render(){
-    const {match} = this.props;
-    const {loading} = this.state;
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
